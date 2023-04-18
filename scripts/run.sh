34a9799c958e475ea0a7c16cf64ee04c3b75afdb
CharaD7 <jijakahn6@gmail.com>
2023-04-18 08:21:24 +0000

    Corrected ewm errors

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,13 +2,11 @@
 
 xrdb merge ~/.Xresources 
 xbacklight -set 10 &
-compton &
+feh --bg-fill ~/Pictures/wallpapers/chad.png
+# compton &
 xset r rate 200 50 &
 picom &
-bubbles &
-eww open eww &
 
-bar &
-fehbg &
+~/.config/chadwm/scripts/bar &
 
 while type dwm >/dev/null; do dwm && continue || break; done
