c94715470d84138cbcd7aff1b17366f58bc10a60
CharaD7 <jijakahn6@gmail.com>
2023-04-19 12:03:12 +0000

    Removed bubbly

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -37,7 +37,8 @@ static const int new_window_attach_on_end = 0; /*  1 means the new window will a
 #define ICONSIZE 22   /* icon size */
 #define ICONSPACING 8 /* space between icon and title */
 
-static const char *fonts[]          = {"Iosevka:style:medium:size=12" ,"JetBrainsMono Nerd Font Mono:style:medium:size=15" };
+static const char *fonts[]          = {"Iosevka:style:medium:size=12" ,"Fira Code iScript:style:medium:size=15" };
+// static const char *fonts[]          = {"Iosevka:style:medium:size=12" ,"JetBrainsMono Nerd Font Mono:style:medium:size=15" };
 
 // theme
 // #include "themes/onedark.h"
@@ -137,7 +138,7 @@ static const Layout layouts[] = {
 static const Key keys[] = {
     /* modifier                         key         function        argument */
 
-    // brightness and audio 
+    // brightness and audio
     {0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol}},
     {0,                       XF86XK_AudioMute, spawn, {.v = mutevol }},
     {0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol}},
@@ -169,7 +170,7 @@ static const Key keys[] = {
     { MODKEY,                           XK_Left,    shiftview,      {.i = -1 } },
     { MODKEY,                           XK_Right,   shiftview,      {.i = +1 } },
 
-    // change m,cfact sizes 
+    // change m,cfact sizes
     { MODKEY,                           XK_h,       setmfact,       {.f = -0.05} },
     { MODKEY,                           XK_l,       setmfact,       {.f = +0.05} },
     { MODKEY|ShiftMask,                 XK_h,       setcfact,       {.f = +0.25} },
@@ -194,7 +195,7 @@ static const Key keys[] = {
     { MODKEY|ControlMask,               XK_o,       incrogaps,      {.i = +1 } },
     { MODKEY|ControlMask|ShiftMask,     XK_o,       incrogaps,      {.i = -1 } },
 
-    // inner+outer hori, vert gaps 
+    // inner+outer hori, vert gaps
     { MODKEY|ControlMask,               XK_6,       incrihgaps,     {.i = +1 } },
     { MODKEY|ControlMask|ShiftMask,     XK_6,       incrihgaps,     {.i = -1 } },
     { MODKEY|ControlMask,               XK_7,       incrivgaps,     {.i = +1 } },
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-xrdb merge ~/.Xresources 
+xrdb merge ~/.Xresources
 xrandr --output HDMI-0 --auto --left-of eDP-1-1
 xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wallpapers/chad.png
+feh --bg-scale ~/Pictures/wallpapers/chad.png
 # compton &
 xset r rate 200 50 &
 picom &
 
 fetch &
 bubbles &
-eww open eww &
 
 ~/.config/chadwm/scripts/bar &
 
