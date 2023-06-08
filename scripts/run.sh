09342265dfb8c021ca3d3c6f926f0038ec1881a5
CharaD7 <jijakahn6@gmail.com>
2023-06-08 08:35:28 +0000

    Added xpad to startup

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ userName=$(echo $USER)
 xrdb merge ~/.Xresources
 xrandr --output HDMI-0 --auto --left-of eDP-1-1
 
-xpad -h
+xpad
 
 chown $userName:$userName /sys/class/backlight/intel_backlight/brightness
 # compton &
