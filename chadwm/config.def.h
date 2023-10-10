dedfa686f04b23baf61806409ce3f59d76af19d5
CharaD7 <jijakahn6@gmail.com>
2023-10-10 15:03:29 +0000

    Enabled tag preview

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -8,12 +8,12 @@ static const unsigned int default_border =
     0; /* to switch back to default border after dynamic border resizing via
           keybinds */
 static const unsigned int snap = 32;   /* snap pixel */
-static const unsigned int gappih = 5; /* horiz inner gap between windows */
-static const unsigned int gappiv = 5; /* vert inner gap between windows */
+static const unsigned int gappih = 10; /* horiz inner gap between windows */
+static const unsigned int gappiv = 10; /* vert inner gap between windows */
 static const unsigned int gappoh =
-    5; /* horiz outer gap between windows and screen edge */
+    10; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    5; /* vert outer gap between windows and screen edge */
+    10; /* vert outer gap between windows and screen edge */
 static const int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning =
@@ -36,7 +36,7 @@ static const int vertpadtab = 35;
 static const int horizpadtabi = 15;
 static const int horizpadtabo = 15;
 static const int scalepreview = 4;
-static const int tag_preview = 0; /* 1 means enable, 0 is off */
+static const int tag_preview = 1; /* 1 means enable, 0 is off */
 static const int colorfultag =
     1; /* 0 means use SchemeSel for selected non vacant tag */
 static const char *upvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "+5%",
