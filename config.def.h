ff0799fa98aa10355ae169cb58f203c3e6b9c613
CharaD7 <jijakahn6@gmail.com>
2024-12-18 09:42:21 +0000

    Added inner gaps

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int default_border = 0; /* to switch back to default border after dynamic border resizing via keybinds */
 static const unsigned int snap = 32;   /* snap pixel */
-static const unsigned int gappih = 0; /* horiz inner gap between windows */
-static const unsigned int gappiv = 0; /* vert inner gap between windows */
+static const unsigned int gappih = 5; /* horiz inner gap between windows */
+static const unsigned int gappiv = 5; /* vert inner gap between windows */
 static const unsigned int gappoh = 5; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov = 5; /* vert outer gap between windows and screen edge */
 static const int smartgaps = 0; /* 1 means no outer gap when there is only one window */
