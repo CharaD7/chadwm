6a9dc84342ee5c06c45b9b5320fbf44862ae91b3
CharaD7 <jijakahn6@gmail.com>
2025-04-17 08:12:52 +0000

    Set frame rate to 60 on launch

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,9 @@ userName=$(echo $USER)
 xrdb merge ~/st/xresources
 
 # use `xrandr --query` to view a list of available displays`
-xrandr --output HDMI-1-0 --auto --left-of eDP-1
+# xrandr --output HDMI-1-0 --auto --left-of eDP-1
+# Set the refresh rate of the primary display to 60Hz
+xrandr --output $(xrandr --query | grep " connected" | awk '{print $1}') --mode $(xrandr | grep -A1 " connected" | tail -n1 | awk '{print $1}') --rate 60
 
 xpad -h &
 
